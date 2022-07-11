a31f08e1a3be7918c55d904fab6a3c21d94db712
po.eng <poeng84@gmail.com>
2022-07-11 14:55:48 +0300

    visible elements

--- src/components/scroll/VirtualScroll.jsx
+++ src/components/scroll/VirtualScroll.jsx
@@ -1,49 +1,71 @@
 import React, { useState, useEffect, useRef } from "react";
 import classes from "./VirtualScroll.module.css";
+
 const VirtualScroll = ({ children, ...props }) => {
+  const PADDING = 0;
+  const HEIGHT_ITEM = 50 + 2 + PADDING;
+  const HEIGHT_CONTAINER = 500;
+  const HIDEN_ITEMS = 4;
+  const HEIGHT_ALL_ITEMS = Math.ceilHEIGHT_ITEM * props.items.length;
+
+  // количество видимых элементов
+  // высота контейнера / высота элемента + 2 пиксела бордюра + отступ + скртые элементы
+
+  const VISIBLE_ITEMS_COUNT =
+    Math.floor(HEIGHT_CONTAINER / HEIGHT_ITEM) + HIDEN_ITEMS;
+  const ref = useRef();
+  let scrollTop = 0;
+
+  let startNode = Math.floor(scrollTop / HEIGHT_ITEM);
+
+  
+  let offsetY = startNode * HEIGHT_ITEM;
+  let visibleItems = [];
   const eventScroll = (e) => {
-    console.log(">>", e.target.scrollTop);
-    console.log(">>", visibleItemsCount);
+    scrollTop = e.target.scrollTop;
+    offsetY = startNode * HEIGHT_ITEM;
+
+
+    console.log("offsetY", offsetY);
+
+    startNode = Math.floor(scrollTop / HEIGHT_ITEM);
+    startNode = Math.max(0, startNode);
+
+    console.log("startNode>", startNode);
+
+
+    visibleItems = props.items.slice(
+      startNode,
+      startNode + VISIBLE_ITEMS_COUNT
+    );
+    console.log("visibleItems", visibleItems);
   };
 
   //
-  const [scrollTop, setScrollTop] = useState(0);
-
-  const heghtItem = 50;
-  const heightContainer = 500;
-  const padding = 10;
-  const ref = useRef();
-  const HIDEN_BENCH = 2;
-
-  let startNode = Math.floor(scrollTop / heghtItem) - 2;
-  const offsetY = startNode * heghtItem;
-
+  // const [scrollTop, setScrollTop] = useState(0);
   //  высота элемента + окантовка + отступ * кол-во элеметов
-  const heightAllItems =
-    Math.ceil(heghtItem + 2 + padding) * props.items.length;
 
-  console.log("heightAllItems", heightAllItems);
-
-  // количество видимых элементов
-  // высота контейнера / высота элемента + 2 пиксела бордюра + отступ
-
-  const visibleItemsCount = Math.floor(
-    heightContainer / (heghtItem + 2 + padding) + HIDEN_BENCH
-  );
-
-  console.log(visibleItemsCount);
+  console.log("heightAllItems", HEIGHT_ALL_ITEMS);
+  console.log(VISIBLE_ITEMS_COUNT);
   return (
     <div
       className={classes.virtual_scroll}
-      style={{ heightContainer }}
+      style={{ HEIGHT_CONTAINER }}
       onScroll={eventScroll}
       ref={ref}
     >
-      {props.items.map((item, index) => (
-        <div className={classes.virtual_item} key={item.id}>
-          {item.name}
-        </div>
-      ))}
+      <div
+        style={{
+          willChange: "transform",
+          transform: `translateY(${offsetY}px)`,
+        }}
+      >
+        {props.items.map((item, index) => (
+          <div className={classes.virtual_item} key={item.id}>
+            {item.name}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
